62611c0dc78061a925e778235caaa294799ad87a
Usman <mu3931368@gmail.com>
2021-12-04 13:16:25 +0500

    my first commit

---
+++ src/First_File.java
@@ -0,0 +1,9 @@
+public class First_File {
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+String name = "Usman";
+String rollno = "20i-2602";
+System.out.println("Name: " + name);
+System.out.println("RollNo: " + rollno);
+}
+}
