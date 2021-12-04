b7699ee708cb983685b33d384e968da01b68d824
usman7384 <88624444+usman7384@users.noreply.github.com>
2021-12-04 13:33:35 +0500

    Update First_File.java

--- src/First_File.java
+++ src/First_File.java
@@ -1,7 +1,7 @@
 public class First_File {
 public static void main(String[] args) {
 // TODO Auto-generated method stub
-String name = "Usman";
+String name = "MuhammadUsman";
 String rollno = "20i-2602";
 System.out.println("Name: " + name);
 System.out.println("RollNo: " + rollno);
